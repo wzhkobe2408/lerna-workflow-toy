b0153626d759c7862208bfa4dec91ed8e6b1a983
wzhkobe2408 <wzhkobe2408@gmail.com>
2020-10-13 16:51:09 +0800

    feat: setup config

--- release.config.js
+++ release.config.js
@@ -1,11 +1,13 @@
 module.exports = {
-  "plugins": [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    ["@semantic-release/git", {
-      "assets": ["dist/**/*.{js,css}", "docs", "package.json"],
-      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    "@semantic-release/npm",
+    "@semantic-release/github"
+    ['@semantic-release/git', {
+      assets: ['dist/**/*.{js,css}', 'docs', 'package.json'],
+      message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
     }]
   ],
-  "branches": ["master"]
+  branches: ['main'],
 }
